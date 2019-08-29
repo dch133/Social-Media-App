9ff9de765206f3b37ce6a4695633158452861aa3
Daniel Chernis <32421770+dch133@users.noreply.github.com>
2019-08-28 20:44:40 -0400

    Add files via upload

---
+++ socialmedia-client/src/redux/store.js
@@ -0,0 +1,26 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import userReducer from './reducers/userReducer';
+import dataReducer from './reducers/dataReducer';
+import uiReducer from './reducers/uiReducer';
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const reducers = combineReducers({
+  user: userReducer,
+  data: dataReducer,
+  UI: uiReducer
+});
+
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(...middleware));
+const store = createStore(reducers, initialState, enhancer);
+
+export default store;
