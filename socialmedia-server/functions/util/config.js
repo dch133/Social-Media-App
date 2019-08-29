e23a02c38fd3e2e7530581f3c84d4fa04773ae8d
Daniel Chernis <32421770+dch133@users.noreply.github.com>
2019-08-28 20:55:06 -0400

    Add files via upload

---
+++ socialmedia-server/functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: "AIzaSyC5UVeN6YnCaEc6BHcOeQkWIvYzRmCBLqI",
+  authDomain: "socialape-7d0b6.firebaseapp.com",
+  databaseURL: "https://socialape-7d0b6.firebaseio.com",
+  projectId: "socialape-7d0b6",
+  storageBucket: "socialape-7d0b6.appspot.com",
+  messagingSenderId: "150679398182",
+  appId: "1:150679398182:web:f3a6ed6d21fa1de7"
+};
