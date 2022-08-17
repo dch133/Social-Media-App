18e471d8437dcb9e92213ecdc97102f747eac2ce
Asifur Rahaman <asifurrahaman754@gmail.com>
2022-08-17 12:49:19 +0600

    index.js file added in pages folder to export all pages in one go

--- socialmedia-client/src/App.js
+++ socialmedia-client/src/App.js
@@ -1,40 +1,37 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
-import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
-import jwtDecode from 'jwt-decode';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import jwtDecode from "jwt-decode";
 // Redux
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import { SET_AUTHENTICATED } from './redux/types';
-import { logoutUser, getUserData } from './redux/actions/userActions';
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import { SET_AUTHENTICATED } from "./redux/types";
+import { logoutUser, getUserData } from "./redux/actions/userActions";
 // Components
-import Navbar from './components/layout/Navbar';
-import themeObject from './util/theme';
-import AuthRoute from './util/AuthRoute';
+import Navbar from "./components/layout/Navbar";
+import themeObject from "./util/theme";
+import AuthRoute from "./util/AuthRoute";
 // Pages
-import home from './pages/home';
-import login from './pages/login';
-import signup from './pages/signup';
-import user from './pages/user';
+import { home, login, signup, user } from "./pages";
 
-import axios from 'axios';
+import axios from "axios";
 
 const theme = createMuiTheme(themeObject);
 
 axios.defaults.baseURL =
-  'https://us-central1-socialape-7d0b6.cloudfunctions.net/api';
+  "https://us-central1-socialape-7d0b6.cloudfunctions.net/api";
 
 const token = localStorage.FBIdToken;
 if (token) {
   const decodedToken = jwtDecode(token);
   if (decodedToken.exp * 1000 < Date.now()) {
     store.dispatch(logoutUser());
-    window.location.href = '/login';
+    window.location.href = "/login";
   } else {
     store.dispatch({ type: SET_AUTHENTICATED });
-    axios.defaults.headers.common['Authorization'] = token;
+    axios.defaults.headers.common["Authorization"] = token;
     store.dispatch(getUserData());
   }
 }
---
+++ socialmedia-client/src/pages/index.js
@@ -0,0 +1,6 @@
+import home from "./home";
+import login from "./login";
+import signup from "./signup";
+import user from "./user";
+
+export { home, login, signup, user };
